04b9b9cb5ec922dc8f002ee1673651b27bda1e0b
chandrakanth-sabnekar <chandrakanth.sabnekar@yahoo.com>
2019-11-02 23:19:20 +0530

    1st commit

--- src/strings/stack.java
+++ src/strings/stack.java
@@ -9,8 +9,7 @@ public class stack {
 
 	public static void main(String args[]){  
 		Stack<String> stack = new Stack<String>();  
-		stack.push("Ayush");  
-		stack.push("Garvit");  
+		
 		stack.push("Amit");  
 		stack.push("Ashish");  
 		stack.push("Garima");  
