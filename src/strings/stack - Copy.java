d40e86d2ead521b7338fb1f40c440120fd5d9449
chandrakanth-sabnekar <chandrakanth.sabnekar@yahoo.com>
2019-11-03 16:27:20 +0530

    developed code

---
+++ src/strings/stack - Copy.java
@@ -0,0 +1,28 @@
+package strings;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.Stack;
+
+public class stack {
+
+	public static void main(String args[]){  
+		Stack<String> stack = new Stack<String>();  
+		
+		stack.push("Amit");  
+		stack.pop();  
+		System.out.Println("Print");
+
+stack.push("Amit");  
+		stack.pop();  
+		System.out.Println("Print");
+		  
+		Set<String> s1 = new HashSet<String>();  
+
+		Iterator<String> itr=stack.iterator();  
+		while(itr.hasNext()){  
+		System.out.println(itr.next());  
+		}  
+		}  
+		}  
