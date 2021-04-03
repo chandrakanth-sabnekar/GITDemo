b490aeefffbf87d56a8aa2a33853c41b64d430b7
chandrakanth-sabnekar <chandrakanth.sabnekar@cubic.com>
2021-04-03 20:22:09 +0530

    Test

--- src/strings/customSort.java
+++ src/strings/customSort.java
@@ -2,9 +2,46 @@ package strings;
 
 public class customSort {
 
+	private String password;
+	private String name;
+
+	public customSort(String password, String name) {
+		this.password = password;
+		this.name = name;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String toString() {
+		return "customSort{" +
+				"password='" + password + '\'' +
+				", name='" + name + '\'' +
+				'}';
+	}
+
+
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+
 	}
+
+
 
 }
